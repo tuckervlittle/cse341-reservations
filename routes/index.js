325efe11fecf356ccc8b1eb622d48d9bf82bc958
DonnyDianderas <donnydianderas@gmail.com>
2025-12-09 00:48:18 -0500

    update logout

--- routes/index.js
+++ routes/index.js
@@ -10,32 +10,6 @@ const swagger = require('./swagger');
 // Authentication routes
 routes.use('/auth', auth);
 
-// Routes for unauthorized emails
-router.get('/not-authorized', (req, res) => {
-  res.status(403).send(`
-    <html>
-      <head>
-        <title>Access Denied</title>
-      </head>
-      <body style="font-family:sans-serif; text-align:center; margin-top:50px;">
-        <h2>Access Denied</h2>
-        <p>You are not authorized. Contact admin to get access.</p>
-        <button onclick="window.location.href='/'" 
-                style="padding:10px 20px; font-size:16px; cursor:pointer;">
-          Go Back
-        </button>
-      </body>
-    </html>
-  `);
-});
-
-// Logout
-router.get('/logout', (req, res) => {
-  req.logout(() => {
-    res.json({ message: 'Logged out' });
-  });
-});
-
 // Home Route
 routes.get('/',
   // #swagger.ignore = true
